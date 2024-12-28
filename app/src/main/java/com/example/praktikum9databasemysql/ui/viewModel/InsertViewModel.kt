d51d1255b1db9789bfe320c0c3439c7d2b6d89be
iamanggi <anggipuspita452@gmail.com>
2024-12-28 15:43:00 +0700

    Menambah fungsi konversi InsertUiEvent ke Mahasiswa

--- app/src/main/java/com/example/praktikum9databasemysql/ui/viewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/viewModel/InsertViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.praktikum9databasemysql.ui.viewModel
 
+import com.example.praktikum9databasemysql.model.Mahasiswa
+
 
 data class InsertUiState(
     val insertUIEvent: InsertUiEvent = InsertUiEvent()
@@ -12,4 +14,13 @@ data class InsertUiEvent(
     val jenisKelamin: String = "",
     val kelas: String = "",
     val angkatan: String = ""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
 )
