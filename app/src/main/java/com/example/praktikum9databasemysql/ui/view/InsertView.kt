be5775d0a404a6ad6bb2446669eb30c32f13b5eb
iamanggi <anggipuspita452@gmail.com>
2024-12-28 16:19:47 +0700

    Menambahkan objek DestinasiEntry untuk route dan title

---
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.praktikum9databasemysql.ui.view
+
+import com.example.praktikum9databasemysql.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
