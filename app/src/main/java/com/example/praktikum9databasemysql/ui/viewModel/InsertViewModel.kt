8f8f3888009666b3264192a5755b96a502324d96
iamanggi <anggipuspita452@gmail.com>
2024-12-28 15:40:19 +0700

    Menambahkan data class InsertUiState dan InsertUiEvent untuk pengelolaan UI

---
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/viewModel/InsertViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.praktikum9databasemysql.ui.viewModel
+
+
+data class InsertUiState(
+    val insertUIEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
