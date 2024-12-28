a1838577e7ff068a2d3defaad33df7f46eff30b3
iamanggi <anggipuspita452@gmail.com>
2024-12-28 16:59:28 +0700

    Menambahkan objek DestinasiHome untuk route dan title

---
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.praktikum9databasemysql.ui.view
+
+import com.example.praktikum9databasemysql.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
