1979b8bc629d0b929f17742e851140dcc999aa91
iamanggi <anggipuspita452@gmail.com>
2024-12-28 16:01:25 +0700

    Menambahkan HomeViewModel dengan fungsi getMhs untuk mengambil data mahasiswa

--- app/src/main/java/com/example/praktikum9databasemysql/ui/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/viewModel/HomeViewModel.kt
@@ -1,10 +1,44 @@
 package com.example.praktikum9databasemysql.ui.viewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.praktikum9databasemysql.model.Mahasiswa
+import com.example.praktikum9databasemysql.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import okio.IOException
 
 sealed class HomeUiState{
     data class  Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState =
+                try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            }
+                catch (e: IOException){
+                    HomeUiState.Error
+                }
+            catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
 }
 
