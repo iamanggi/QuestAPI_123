e822075b6cdf5da56f14035c27b725e0dfeb9e13
iamanggi <anggipuspita452@gmail.com>
2024-12-28 16:57:59 +0700

    Membuat EntryMhsScreen dengan form input data mahasiswa dan tombol simpan

--- app/src/main/java/com/example/praktikum9databasemysql/ui/view/InsertView.kt
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/view/InsertView.kt
@@ -1,19 +1,31 @@
 package com.example.praktikum9databasemysql.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum9databasemysql.ui.customwidget.CostumeTopAppBar
 import com.example.praktikum9databasemysql.ui.navigation.DestinasiNavigasi
+import com.example.praktikum9databasemysql.ui.viewModel.InsertUiEvent
+import com.example.praktikum9databasemysql.ui.viewModel.InsertUiState
 import com.example.praktikum9databasemysql.ui.viewModel.InsertViewModel
 import com.example.praktikum9databasemysql.ui.viewModel.PenyediaViewModel
 import kotlinx.coroutines.launch
@@ -58,4 +70,102 @@ val coroutineScope = rememberCoroutineScope()
         )
     }
 }
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent)-> Unit,
+    onSaveClick:()->Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = insertUiState.insertUIEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent)-> Unit ={},
+    enabled: Boolean = true
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = {onValueChange(insertUiEvent.copy(nama = it))},
+            label = { Text("Nama")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.nim,
+            onValueChange = {onValueChange(insertUiEvent.copy(nim = it))},
+            label = { Text("Nim")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.jenisKelamin,
+            onValueChange = {onValueChange(insertUiEvent.copy(jenisKelamin = it))},
+            label = { Text("Jenis Kelamin")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.alamat,
+            onValueChange = {onValueChange(insertUiEvent.copy(alamat = it))},
+            label = { Text("Alamat")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.kelas,
+            onValueChange = {onValueChange(insertUiEvent.copy(kelas = it))},
+            label = { Text("Kelas")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.angkatan,
+            onValueChange = {onValueChange(insertUiEvent.copy(angkatan = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text("Angkatan")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled){
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
