b549ad79186e8ca166cad195c623c0304e2eba38
iamanggi <anggipuspita452@gmail.com>
2024-12-30 02:44:21 +0700

    Menambahkan initializer untuk DetailViewModel dan UpdateViewModel

--- app/src/main/java/com/example/praktikum9databasemysql/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/viewModel/PenyediaViewModel.kt
@@ -5,11 +5,14 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.lifecycle.viewmodel.initializer
 import com.example.praktikum9databasemysql.MahasiswaApplications
+import com.example.praktikum9databasemysql.viewmodel.UpdateViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
     initializer{HomeViewModel(MahasiswaApp().container.mahasiswaRepository)}
         initializer{InsertViewModel(MahasiswaApp().container.mahasiswaRepository)}
+        initializer{DetailViewModel(MahasiswaApp().container.mahasiswaRepository)}
+        initializer{ UpdateViewModel(MahasiswaApp().container.mahasiswaRepository) }
     }
 
     fun CreationExtras.MahasiswaApp(): MahasiswaApplications =
