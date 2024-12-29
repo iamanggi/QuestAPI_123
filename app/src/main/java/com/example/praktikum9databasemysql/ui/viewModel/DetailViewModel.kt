773aa344aa81d1ffb2663a012fa413b744b6257f
iamanggi <anggipuspita452@gmail.com>
2024-12-29 23:50:48 +0700

    Menambahkan fungsi untuk mendapatkan detail mahasiswa berdasarkan nim

---
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/viewModel/DetailViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.praktikum9databasemysql.ui.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum9databasemysql.model.Mahasiswa
+import com.example.praktikum9databasemysql.repository.MahasiswaRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class DetailViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+
+    private val _mahasiswa = MutableStateFlow<Mahasiswa?>(null)
+    val mahasiswa: StateFlow<Mahasiswa?> get() = _mahasiswa
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> get() = _errorMessage
+
+    fun getMahasiswaById(nim: String) {
+        viewModelScope.launch {
+            try {
+                val mahasiswaList = mhs.getMahasiswa()
+                val mahasiswa = mahasiswaList.firstOrNull { it.nim == nim }
+
+                if (mahasiswa != null) {
+                    _mahasiswa.value = mahasiswa
+                } else {
+                    _errorMessage.value = "Mahasiswa dengan NIM $nim tidak ditemukan."
+                }
+            } catch (e: Exception) {
+                _errorMessage.value = "Gagal memuat data mahasiswa: ${e.message}"
+            }
+        }
+    }
+}
