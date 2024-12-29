b9d08f07c0294e2aa136b7cd23c7da620a932fa3
iamanggi <anggipuspita452@gmail.com>
2024-12-30 00:12:01 +0700

    Menambahkan composable DetailCard dan DetailRow untuk menampilkan detail mahasiswa

--- app/src/main/java/com/example/praktikum9databasemysql/ui/view/DetailView.kt
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/view/DetailView.kt
@@ -1,8 +1,69 @@
 package com.example.praktikum9databasemysql.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.praktikum9databasemysql.model.Mahasiswa
 import com.example.praktikum9databasemysql.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "detail"
     override val titleRes = "Detail Mahasiswa"
+}
+
+@Composable
+fun DetailCard(mahasiswa: Mahasiswa) {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            DetailRow(label = "Nama", value = mahasiswa.nama)
+            DetailRow(label = "Nim", value = mahasiswa.nim)
+            DetailRow(label = "Kelas", value = mahasiswa.kelas)
+            DetailRow(label = "Alamat", value = mahasiswa.alamat)
+            DetailRow(label = "Angkatan", value = mahasiswa.angkatan)
+        }
+    }
+}
+
+@Composable
+fun DetailRow(label: String, value: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = ":",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.weight(0.1f)
+        )
+        Spacer(modifier = Modifier.padding(start = 10.dp))
+        Text(
+            text = value,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.weight(2f)
+        )
+    }
 }
