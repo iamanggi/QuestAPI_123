8946fc1d6a60d8d5e9409cdf240f2df315eb8bdd
iamanggi <anggipuspita452@gmail.com>
2024-12-30 00:05:25 +0700

    Menambahkan objek DestinasiDetail untuk navigasi ke halaman detail mahasiswa

---
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/view/DetailView.kt
@@ -0,0 +1,8 @@
+package com.example.praktikum9databasemysql.ui.view
+
+import com.example.praktikum9databasemysql.ui.navigation.DestinasiNavigasi
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Mahasiswa"
+}
