8dcd28ae87b9fe3cc87dd1ec75a892f164f8a1f7
iamanggi <anggipuspita452@gmail.com>
2024-12-29 10:56:00 +0700

    Menambahkan fungsi HomeScreen untuk menampilkan daftar Mahasiswa, detail, dan penghapusan data

--- app/src/main/java/com/example/praktikum9databasemysql/ui/view/HomeView.kt
+++ app/src/main/java/com/example/praktikum9databasemysql/ui/view/HomeView.kt
@@ -15,30 +15,84 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum9databasemysql.R
 import com.example.praktikum9databasemysql.model.Mahasiswa
+import com.example.praktikum9databasemysql.ui.customwidget.CostumeTopAppBar
 import com.example.praktikum9databasemysql.ui.navigation.DestinasiNavigasi
 import com.example.praktikum9databasemysql.ui.viewModel.HomeUiState
+import com.example.praktikum9databasemysql.ui.viewModel.HomeViewModel
+import com.example.praktikum9databasemysql.ui.viewModel.PenyediaViewModel
 
 object DestinasiHome: DestinasiNavigasi {
     override val route = "home"
     override val titleRes = "Home Mhs"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: HomeViewModel =viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMhs()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add,
+                    contentDescription = "Add Mahasiswa")
+            }
+        },
+    ) {  innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.mhsUiState,
+            retryAction = { viewModel.getMhs()},
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deleteMhs(it.nim)
+                viewModel.getMhs()
+            }
+        )
+    }
+}
 
 @Composable
 fun HomeStatus(
@@ -82,7 +136,7 @@ fun OnLoading(
 ){
     Image(
         modifier = modifier.size(200.dp),
-        painter = painterResource(R.drawable.loading_img),
+        painter = painterResource(R.drawable.loading),
         contentDescription = stringResource(R.string.loading)
     )
 }
@@ -98,7 +152,7 @@ fun OnError(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(painter = painterResource(id = R.drawable.ic_connection_error),
+        Image(painter = painterResource(id = R.drawable.connection_error),
             contentDescription = "")
         Text(text = stringResource(R.string.loading_failed),
             modifier = Modifier.padding(16.dp))
